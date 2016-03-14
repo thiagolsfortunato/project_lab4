624b3fc820efb88c9b598f69a12b4e14bb37029d
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-14 11:11:18 -0300

    Update Class GroupPaperDTOConverter

--- first_project/src/br/com/fatec/firstProject/core/converter/GroupPaperDTOConverter.java
+++ first_project/src/br/com/fatec/firstProject/core/converter/GroupPaperDTOConverter.java
@@ -1,5 +1,49 @@
 package br.com.fatec.firstProject.core.converter;
 
-public class GroupPaperDTOConverter {
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import br.com.fatec.firstProject.api.dto.GroupPaperDTO;
+import br.com.fatec.firstProject.api.entity.GroupPaper;
+import br.com.spektro.minispring.dto.DTOConverter;
+
+public class GroupPaperDTOConverter implements DTOConverter<GroupPaper, GroupPaperDTO> {
+
+	@Override
+	public GroupPaperDTO toDTO(GroupPaper groupPaper) {
+		GroupPaperDTO groupPaperDTo = new GroupPaperDTO();
+		groupPaperDTo.setId(groupPaper.getId());
+		groupPaperDTo.setName(groupPaper.getName());
+		groupPaperDTo.setDescription(groupPaper.getDescription());
+		return groupPaperDTo;
+	}
+
+	@Override
+	public GroupPaper toEntity(GroupPaperDTO groupPaperDTO) {
+		GroupPaper groupPaper = new GroupPaper();
+		groupPaper.setId(groupPaperDTO.getId());
+		groupPaper.setName(groupPaperDTO.getName());
+		groupPaper.setDescription(groupPaperDTO.getDescription());
+		return groupPaper;
+	}
+
+	@Override
+	public List<GroupPaperDTO> toDTO(List<GroupPaper> listGroupPaper) {
+		List<GroupPaperDTO> groupPaperDTO = Lists.newArrayList();
+		for (GroupPaper groupPaper : listGroupPaper) {
+			groupPaperDTO.addAll(this.toDTO(listGroupPaper));
+		}
+		return groupPaperDTO;
+	}
+	
+	@Override
+	public List<GroupPaper> toEntity(List<GroupPaperDTO> listGroupPaperDTO) {
+		List<GroupPaper> groupPaper = Lists.newArrayList();
+		for(GroupPaperDTO groupPaperDTO : listGroupPaperDTO){
+			groupPaper.add(this.toEntity(groupPaperDTO));
+		}
+		return groupPaper;
+	}
 
 }
